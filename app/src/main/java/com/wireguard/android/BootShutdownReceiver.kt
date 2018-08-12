1f97df21ef902564e8ccc1d7f02cc2541ffbd653
Harsh Shandilya <harsh@prjkt.io>
2018-08-12 15:36:59 +0530

    Rewrite boot receiver in Kotlin
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

---
+++ app/src/main/java/com/wireguard/android/BootShutdownReceiver.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2018 Samuel Holland <samuel@sholland.org>
+ * Copyright © 2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.wireguard.android
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+import com.wireguard.android.backend.WgQuickBackend
+import com.wireguard.android.util.ExceptionLoggers
+
+class BootShutdownReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == null)
+            return
+        Application.backendAsync.thenAccept { backend ->
+            if (backend !is WgQuickBackend)
+                return@thenAccept
+            val action = intent.action
+            val tunnelManager = Application.getTunnelManager()
+            if (Intent.ACTION_BOOT_COMPLETED == action) {
+                Log.i(TAG, "Broadcast receiver restoring state (boot)")
+                tunnelManager!!.restoreState(false).whenComplete(ExceptionLoggers.D)
+            } else if (Intent.ACTION_SHUTDOWN == action) {
+                Log.i(TAG, "Broadcast receiver saving state (shutdown)")
+                tunnelManager!!.saveState()
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = "WireGuard/" + BootShutdownReceiver::class.java.simpleName
+    }
+}
