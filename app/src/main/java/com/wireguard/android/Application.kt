msfjarvis/viscerion

commit b2a559d1244ba8415ce16f2ab982b063b47fce0e

Disable ACRA for now
This is still going to ZX2C4's servers atm which is undesirable.
Disabling it until I roll out my own solution.

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/android/Application.kt
@@ -63,11 +63,13 @@ class Application : android.app.Application() {
             System.exit(0)
         }
 
+        /*
         val installSource = getInstallSource(context)
         if (installSource != null) {
             ACRA.init(this)
             ACRA.getErrorReporter().putCustomData("installSource", installSource)
         }
+        */
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -96,6 +98,7 @@ class Application : android.app.Application() {
         tunnelManager = TunnelManager(FileConfigStore(applicationContext))
         tunnelManager.onCreate()
 
+        /*
         if (sharedPreferences.getBoolean("enable_logging", true)) {
             val configurationBuilder = CoreConfigurationBuilder(this)
 
@@ -123,6 +126,7 @@ class Application : android.app.Application() {
                 }
             }
         }
+        */
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             createNotificationChannel()

app/src/main/res/xml/preferences.xml
@@ -9,10 +9,12 @@
     <com.wireguard.android.preference.ToolsInstallerPreference android:key="tools_installer" />
     <com.wireguard.android.preference.ZipExporterPreference />
     <com.wireguard.android.preference.LogExporterPreference />
+<!--
     <CheckBoxPreference
         android:defaultValue="true"
         android:key="enable_logging"
         android:summaryOn="@string/logging_pref_summary_on"
         android:summaryOff="@string/logging_pref_summary_off"
         android:title="@string/logging_pref_title" />
+-->
 </PreferenceScreen>

commit b4f5efc1c5665371bdb6d3cc6a0538f385431bc1

Good lord am I dumb
When I disabled ACRA in commit b2a559d1244b, I inadvertently
also commented out the asyncWorker call that actually sets
up the app with a backend to work with, causing all kinds
of shit (_obviously_) to go down, like tunnels never going
up, version preference never detecting the backend, and
wg-quick specific preferences never showing.

Fixes: b2a559d1244b ("Disable ACRA for now")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/android/Application.kt
@@ -98,8 +98,8 @@ class Application : android.app.Application() {
         tunnelManager = TunnelManager(FileConfigStore(applicationContext))
         tunnelManager.onCreate()
 
+        if (sharedPreferences.getBoolean("enable_logging", false)) {
         /*
-        if (sharedPreferences.getBoolean("enable_logging", true)) {
             val configurationBuilder = CoreConfigurationBuilder(this)
 
             // Core configuration
@@ -116,17 +116,18 @@ class Application : android.app.Application() {
                     .setHttpMethod(HttpSender.Method.POST)
                     .setCompress(true)
             ACRA.init(this, configurationBuilder)
-
+        */
             asyncWorker!!.supplyAsync<Backend> { getBackend() }.thenAccept { backend ->
                 futureBackend.complete(backend)
+                /*
                 if (ACRA.isInitialised()) {
                     ACRA.getErrorReporter().putCustomData("backend", backend.javaClass.simpleName)
                     asyncWorker!!.supplyAsync<String> { backend.version }
                             .thenAccept { version -> ACRA.getErrorReporter().putCustomData("backendVersion", version) }
                 }
+                */
             }
         }
-        */
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
             createNotificationChannel()