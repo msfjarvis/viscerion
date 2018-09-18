92c7df4363f8a958a641e51bc9542eb4d5f65222
Harsh Shandilya <harsh@prjkt.io>
2018-09-18 15:22:23 +0530

    Cleanup widgets
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/src/main/java/com/wireguard/android/widget/MultiselectableRelativeLayout.kt
+++ app/src/main/java/com/wireguard/android/widget/MultiselectableRelativeLayout.kt
@@ -14,18 +14,11 @@ import com.wireguard.android.R
 class MultiselectableRelativeLayout : RelativeLayout {
     private var multiselected: Boolean = false
 
-    constructor(context: Context) : super(context) {}
+    constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
-        context,
-        attrs,
-        defStyleAttr,
-        defStyleRes
-    )
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     override fun onCreateDrawableState(extraSpace: Int): IntArray {
         if (multiselected) {
