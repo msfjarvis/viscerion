msfjarvis/viscerion

commit becb41c12fb17cff532b51eca3a6c81d61dfd35e

Fix tunnel deletion semantics
This was so stupid I should consider suicide

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.kt
@@ -245,10 +245,8 @@ class TunnelListFragment : BaseFragment() {
         if (binding == null)
             return
         Application.tunnelManager.getTunnels().thenAccept { tunnels ->
-            if (newTunnel != null)
-                viewForTunnel(newTunnel, tunnels)!!.setSingleSelected(true)
-            if (oldTunnel != null)
-                viewForTunnel(oldTunnel, tunnels)!!.setSingleSelected(false)
+            newTunnel ?: viewForTunnel(newTunnel, tunnels)!!.setSingleSelected(true)
+            oldTunnel ?: viewForTunnel(oldTunnel, tunnels)!!.setSingleSelected(false)
         }
     }
 
@@ -369,9 +367,9 @@ class TunnelListFragment : BaseFragment() {
                     Application.tunnelManager.getTunnels().thenAccept { tunnels ->
                         val tunnelsToDelete = ArrayList<Tunnel>()
                         for (position in copyCheckedItems)
-                            tunnelsToDelete.add(tunnels[position!!])
+                            tunnelsToDelete.add(tunnels[position])
 
-                        val futures = KotlinCompanions.streamForDeletion(tunnels)
+                        val futures = KotlinCompanions.streamForDeletion(tunnelsToDelete)
                         CompletableFuture.allOf(*futures)
                             .thenApply { futures.size }
                             .whenComplete { count, throwable ->

app/src/main/java/com/wireguard/android/util/KotlinCompanions.java
@@ -12,6 +12,7 @@ import java9.util.concurrent.CompletableFuture;
 import java9.util.concurrent.CompletionStage;
 import java9.util.stream.StreamSupport;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -31,7 +32,7 @@ public final class KotlinCompanions {
                 .toArray(CompletableFuture[]::new));
     }
 
-    public static CompletableFuture[] streamForDeletion(final ObservableSortedKeyedList<String, Tunnel> tunnels) {
+    public static CompletableFuture[] streamForDeletion(final ArrayList<Tunnel> tunnels) {
         return StreamSupport.stream(tunnels)
                 .map(Tunnel::delete)
                 .toArray(CompletableFuture[]::new);

commit 085837759534c762fc13bb40050fc3fef43a88ce

TunnelListFragment: Revert wrong use of elvis operator
Fixes: becb41c12fb1 ("Fix tunnel deletion semantics")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.kt
@@ -245,8 +245,10 @@ class TunnelListFragment : BaseFragment() {
         if (binding == null)
             return
         Application.tunnelManager.getTunnels().thenAccept { tunnels ->
-            newTunnel ?: viewForTunnel(newTunnel, tunnels)!!.setSingleSelected(true)
-            oldTunnel ?: viewForTunnel(oldTunnel, tunnels)!!.setSingleSelected(false)
+            if (newTunnel != null)
+                viewForTunnel(newTunnel, tunnels)!!.setSingleSelected(true)
+            if (oldTunnel != null)
+                viewForTunnel(oldTunnel, tunnels)!!.setSingleSelected(false)
         }
     }
 