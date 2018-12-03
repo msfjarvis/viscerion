fce051e6325c80a9460d3be28cafa08153af272c
Harsh Shandilya <msfjarvis@gmail.com>
2018-12-03 14:41:21 +0530

    KotlinCompanions: Remove unnecessarily duplicated method
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/wireguard/android/util/KotlinCompanions.java
+++ app/src/main/java/com/wireguard/android/util/KotlinCompanions.java
@@ -5,7 +5,6 @@
 
 package com.wireguard.android.util;
 
-import com.wireguard.android.Application;
 import com.wireguard.android.model.Tunnel;
 import com.wireguard.android.model.TunnelManager;
 import java9.util.concurrent.CompletableFuture;
@@ -28,7 +27,7 @@ public final class KotlinCompanions {
                                                              final TunnelManager tunnelManager) {
         return CompletableFuture.allOf(StreamSupport.stream(tunnels)
                 .filter(tunnel -> previouslyRunning.contains(tunnel.getName()))
-                .map(tunnel -> setTunnelState(tunnel, tunnelManager))
+                .map(tunnel -> tunnelManager.setTunnelState(tunnel, Tunnel.State.UP))
                 .toArray(CompletableFuture[]::new));
     }
 
@@ -38,23 +37,4 @@ public final class KotlinCompanions {
                 .toArray(CompletableFuture[]::new);
     }
 
-    private static CompletionStage<Tunnel.State> setTunnelState(final Tunnel tunnel, final TunnelManager tunnelManager) {
-        // Ensure the configuration is loaded before trying to use it.
-        return tunnel.getConfigAsync().thenCompose(x ->
-                Application.Companion.getAsyncWorker().supplyAsync(() -> {
-                    try {
-                        return Application.Companion.getBackend().setState(tunnel, Tunnel.State.UP);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                })
-        ).whenComplete((newState, e) -> {
-            // Ensure onStateChanged is always called (failure or not), and with the correct state.
-            tunnel.onStateChanged(e == null ? newState : tunnel.getState());
-            if (e == null && newState == Tunnel.State.UP)
-                tunnelManager.setLastUsedTunnel(tunnel);
-            tunnelManager.saveState();
-        });
-    }
 }
