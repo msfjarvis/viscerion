e618d55345a4b7b5e6564c2820b7d5a29d2fa98b
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-31 18:12:56 +0530

    release: Build bundles too
    skip-ci
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- _bin/release.sh
+++ _bin/release.sh
@@ -3,5 +3,5 @@
 [ -z "$(command -v hub)" ] && { echo "hub not installed; aborting!"; exit 1; }
 TAG="${1}"
 hub tag -a "${TAG:?}"
-./gradlew clean assembleRelease
+./gradlew clean bundleRelease assembleRelease
 hub release create "${TAG}" -a app/build/outputs/apk/release/wg-android_"${TAG}"-release.apk
