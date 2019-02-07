defd5e940fcd751b52e57a8635e8a39982d522f7
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-07 18:04:43 +0530

    Dependency updates
    - AGP
    - Databinding runtime and compiler
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0")
+        classpath("com.android.tools.build:gradle:3.3.1")
         classpath(kotlin("gradle-plugin", "1.3.21"))
         classpath("com.diffplug.spotless:spotless-plugin-gradle:3.17.0")
     }
