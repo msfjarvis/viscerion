00249bb7dd94ab34d2c2f7a6c29df0579ae3975f
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-13 00:43:36 +0530

    ApplicationPreferences: Codestyle nits
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/wireguard/android/util/ApplicationPreferences.kt
+++ app/src/main/java/com/wireguard/android/util/ApplicationPreferences.kt
@@ -7,32 +7,28 @@ package com.wireguard.android.util
 import androidx.core.content.edit
 import com.wireguard.android.Application
 
-class ApplicationPreferences {
-    companion object {
-        const val appThemeKey = "dark_theme"
-        const val globalExclusionsKey = "global_exclusions"
-        const val forceUserspaceBackendkey = "force_userspace_backend"
-        const val whitelistAppsKey = "whitelist_exclusions"
-        var exclusions: String
-            get() {
-                return Application.sharedPreferences.getString(globalExclusionsKey, "") as String
+object ApplicationPreferences {
+    const val appThemeKey = "dark_theme"
+    const val globalExclusionsKey = "global_exclusions"
+    const val forceUserspaceBackendkey = "force_userspace_backend"
+    const val whitelistAppsKey = "whitelist_exclusions"
+    var exclusions: String
+        get() = Application.sharedPreferences.getString(globalExclusionsKey, "") ?: ""
+        set(value) {
+            Application.sharedPreferences.edit {
+                putString(globalExclusionsKey, value)
             }
-            set(value) {
-                Application.sharedPreferences.edit {
-                    putString(globalExclusionsKey, value)
-                }
-                exclusionsArray = value.toArrayList().toCollection(ArrayList())
-            }
-        var exclusionsArray: ArrayList<String> = exclusions.toArrayList()
-            private set
+            exclusionsArray = value.toArrayList().toCollection(ArrayList())
+        }
+    var exclusionsArray: ArrayList<String> = exclusions.toArrayList()
+        private set
 
-        val useDarkTheme: Boolean
-            get() = Application.sharedPreferences.getBoolean(appThemeKey, false)
+    val useDarkTheme: Boolean
+        get() = Application.sharedPreferences.getBoolean(appThemeKey, false)
 
-        val forceUserspaceBackend: Boolean
-            get() = Application.sharedPreferences.getBoolean(forceUserspaceBackendkey, false)
+    val forceUserspaceBackend: Boolean
+        get() = Application.sharedPreferences.getBoolean(forceUserspaceBackendkey, false)
 
-        val whitelistApps: Boolean
-            get() = Application.sharedPreferences.getBoolean(whitelistAppsKey, false)
-    }
+    val whitelistApps: Boolean
+        get() = Application.sharedPreferences.getBoolean(whitelistAppsKey, false)
 }
