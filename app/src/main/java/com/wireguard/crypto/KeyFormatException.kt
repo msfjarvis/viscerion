7f5a5006b0e496fabf79b8942eff081a88bf32fb
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-17 08:27:46 +0530

    Modify license headers
    skip-ci
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/wireguard/crypto/KeyFormatException.kt
+++ app/src/main/java/com/wireguard/crypto/KeyFormatException.kt
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2019 Harsh Shandilya. All Rights Reserved.
+ * Copyright © 2017-2018 WireGuard LLC.
+ * Copyright © 2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.wireguard.crypto
--- spotless.license.kt
+++ spotless.license.kt
@@ -1,4 +1,5 @@
 /*
- * Copyright © 2019 Harsh Shandilya. All Rights Reserved.
+ * Copyright © 2017-2018 WireGuard LLC.
+ * Copyright © 2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
