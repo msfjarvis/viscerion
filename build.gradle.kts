5f05d3b1ff172ce59a2e06b0e837582e4d62f834
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-17 10:08:52 +0530

    build: Fix dependencyUpdates configuration
    Fix codestyle issues and set report type to JSON
    
    skip-ci
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,20 +24,22 @@ allprojects {
 }
 
 tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
-    checkForGradleUpdate = false
-        resolutionStrategy {
-            componentSelection {
-                all {
-                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
-                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
-                        .any { it.matches(candidate.version)
-                }
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                    .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                    .any { it.matches(candidate.version) }
                 if (rejected) {
                     reject("Release candidate")
                 }
             }
         }
     }
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
 
 tasks {
