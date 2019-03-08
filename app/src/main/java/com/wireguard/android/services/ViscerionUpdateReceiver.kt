8b92fb849e43c3264e5787eef8ddf641d72b991e
Harsh Shandilya <msfjarvis@gmail.com>
2019-03-08 12:44:51 +0530

    Export ZIPs when the app is updated
    It was brought to my attention that when I pushed the migration update to
    the WireGuard-KT, the undimissible dialog caused many users to be
    locked out of the app with no way to export their configs, making for a
    very disastrous blunder on my end causing a rather bad UX for users.
    
    To avoid something like it happening again, install an update receiver
    that exports all configurations each time the app is updated so broken
    updates can be rolled back without trouble. I hope nobody has to rely on
    this ever, but I'm only human and things can always slip by. This does
    not fire on debugging builds since I install way too often and the IO
    penalty is not worth it.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ app/src/main/java/com/wireguard/android/services/ViscerionUpdateReceiver.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2017-2018 WireGuard LLC.
+ * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.services
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import com.wireguard.android.Application
+import com.wireguard.android.BuildConfig
+import com.wireguard.android.util.ZipExporter
+import timber.log.Timber
+
+class ViscerionUpdateReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent == null || intent.action == null || BuildConfig.DEBUG) return
+        if (context != null &&
+            ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            return
+        Application.asyncWorker.runAsync {
+            Application.tunnelManager.getTunnels().thenAccept {
+                ZipExporter.exportZip(it) { filePath, throwable ->
+                    Timber.tag("ViscerionUpdate")
+                    if (throwable == null) {
+                        Timber.d("Successfully exported ZIPs to $filePath")
+                    } else {
+                        Timber.d(throwable)
+                    }
+                }
+            }
+        }
+    }
+}
