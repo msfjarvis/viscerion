0f25a6e5300d6fcbd8450115fe69d72376db5ccb
Harsh Shandilya <msfjarvis@gmail.com>
2019-03-09 23:16:15 +0530

    Remove useless suppression
    [skip travis]
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/wireguard/android/widget/fab/FloatingActionButtonBehavior.kt
+++ app/src/main/java/com/wireguard/android/widget/fab/FloatingActionButtonBehavior.kt
@@ -3,8 +3,6 @@
  * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-@file:Suppress("Unused")
-
 package com.wireguard.android.widget.fab
 
 import android.content.Context
