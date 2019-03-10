548c5b9e94db9bb6f3a6c77d93df407ae641e65c
Harsh Shandilya <msfjarvis@gmail.com>
2019-03-10 16:19:58 +0530

    Revert "[REVERTME] Interface: Swallow kill-switch commands for now"
    The command line tools are configured to choke on invalid configuration
    options and abort execution, so even if you could actually import the
    configurations, tunnels can not be brought up. Reverting the patch so
    that we fail fast rather than have unusable tunnels.
    
    This reverts commit ac0799c446ec700c2f5d16784d9e67f3328621cb.

--- app/src/main/java/com/wireguard/config/Interface.kt
+++ app/src/main/java/com/wireguard/config/Interface.kt
@@ -13,7 +13,6 @@ import com.wireguard.config.BadConfigException.Section
 import com.wireguard.crypto.Key
 import com.wireguard.crypto.KeyFormatException
 import com.wireguard.crypto.KeyPair
-import timber.log.Timber
 import java.net.InetAddress
 import java.util.Collections
 import java.util.LinkedHashSet
@@ -325,9 +324,6 @@ class Interface private constructor(builder: Builder) {
                     "listenport" -> builder.parseListenPort(attribute.value)
                     "mtu" -> builder.parseMtu(attribute.value)
                     "privatekey" -> builder.parsePrivateKey(attribute.value)
-                    "postup", "postdown", "preup", "predown" -> {
-                        Timber.tag("Interface").d("Kill-switch attribute encountered, ignoring...")
-                    }
                     else -> throw BadConfigException(
                         Section.INTERFACE, Location.TOP_LEVEL,
                         Reason.UNKNOWN_ATTRIBUTE, attribute.key
