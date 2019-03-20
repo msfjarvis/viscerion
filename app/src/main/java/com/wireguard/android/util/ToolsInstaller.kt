msfjarvis/viscerion

commit fdc5b5fadee4a07654699d77a083da3a7d09518d

ToolsInstaller: Revamp Magisk version detection
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/util/ToolsInstaller.kt
@@ -28,6 +28,7 @@ class ToolsInstaller(val context: Context) {
     private var installAsMagiskModule: Boolean? = null
 
     init {
+        Timber.tag("ToolsInstaller").d("SU version: $magiskDir")
         nativeLibraryDir = if (context.applicationInfo.splitSourceDirs.isNullOrEmpty()) {
             File(context.applicationInfo.nativeLibraryDir)
         } else {
@@ -206,11 +207,11 @@ class ToolsInstaller(val context: Context) {
         private fun getMagiskDirectory(): String {
             val output = ArrayList<String>()
             Application.rootShell.run(output,
-                    "su --version | cut -d ':' -f 1 | cut -d '-' -f 1")
-            val magiskVer = output[0].toDoubleOrNull()
+                    "su -V")
+            val magiskVer = output[0].toInt()
             return when (magiskVer) {
-                18.0, 18.1 -> "/sbin/.magisk/img"
-                18.2 -> "/data/adb/modules"
+                in(18000..18100) -> "/sbin/.magisk/img"
+                in(18101..18200) -> "/data/adb/modules"
                 else -> "/sbin/.core/img"
             }
         }

commit 2bf22dbc5981a1d338f36ed3e3757bb655b3d993

ToolsInstaller: Remove log print
This was accidentally committed with fdc5b5fadee4 which
causes the Application class to fail loading because this
throws an exception on unrooted devices.

Fixes: fdc5b5fadee4 ("ToolsInstaller: Revamp Magisk version detection")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/util/ToolsInstaller.kt
@@ -28,7 +28,6 @@ class ToolsInstaller(val context: Context) {
     private var installAsMagiskModule: Boolean? = null
 
     init {
-        Timber.tag("ToolsInstaller").d("SU version: $magiskDir")
         nativeLibraryDir = if (context.applicationInfo.splitSourceDirs.isNullOrEmpty()) {
             File(context.applicationInfo.nativeLibraryDir)
         } else {