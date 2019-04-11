0e38010ed3762f9095a9985f4f8788f889ba2f75
Harsh Shandilya <msfjarvis@gmail.com>
2019-04-11 22:02:49 +0530

    buildSrc: Fix spotless build
    skip-ci
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildSrc/src/main/kotlin/DependencyStore.kt
+++ buildSrc/src/main/kotlin/DependencyStore.kt
@@ -3,8 +3,8 @@
  * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+@file:Suppress("Unused")
 
-@Suppress("Unused")
 class DependencyStore {
 
     object AndroidX {
--- buildSrc/src/main/kotlin/VersionConfiguration.kt
+++ buildSrc/src/main/kotlin/VersionConfiguration.kt
@@ -3,8 +3,8 @@
  * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+@file:Suppress("Unused")
 
-@Suppress("Unused")
 object VersionConfiguration {
     const val versionCode = 4000
     const val versionName = "4.0.0"
