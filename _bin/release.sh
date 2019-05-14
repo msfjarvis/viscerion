8199975de1cbe163720eb0087815b1431dedcd27
Harsh Shandilya <msfjarvis@gmail.com>
2019-05-15 03:53:07 +0530

    Update release script
    [ci skip]
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- _bin/release.sh
+++ _bin/release.sh
@@ -5,5 +5,5 @@ trap 'exit 1' SIGINT SIGTERM
 [ -z "$(command -v hub)" ] && { echo "hub not installed; aborting!"; exit 1; }
 TAG="${1}"
 hub tag -a "${TAG:?}"
-./gradlew clean bundleRelease assembleRelease
-hub release create "${TAG}" -a app/build/outputs/apk/release/viscerion_"${TAG}"-release.apk
+./gradlew clean bundlePieRelease assemblePieRelease assembleQuinoaRelease
+hub release create "${TAG}" -a app/build/outputs/apk/release/viscerion_"${TAG}"-pie-release.apk -a app/build/outputs/apk/release/viscerion_"${TAG}"-quinoa-release.apk
