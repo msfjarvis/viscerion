b2e937a866af587effffd5aa7b988a26c1865e3b
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-04 05:23:27 +0530

    Dependency updates
    ThreeTenABP 1.2.1
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildSrc/src/main/kotlin/DependencyStore.kt
+++ buildSrc/src/main/kotlin/DependencyStore.kt
@@ -39,7 +39,7 @@ class DependencyStore {
         private const val inlineActivityResultVersion = "0.1.0"
         private const val koinVersion = "2.0.1"
         private const val retroFutureVersion = "1.7.1"
-        private const val threetenabpVersion = "1.2.0"
+        private const val threetenabpVersion = "1.2.1"
         private const val timberVersion = "4.7.1"
         private const val zxingVersion = "3.4.0"
 
