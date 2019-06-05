5b2ae69f2a563a035f5e07a2c210acec7ddab130
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-06 04:35:07 +0530

    Rollback AGP and Databinding to previous versions
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         maven(url = "https://storage.googleapis.com/r8-releases/raw")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0-alpha02")
+        classpath("com.android.tools.build:gradle:3.5.0-beta03")
         classpath(kotlin("gradle-plugin", "1.3.31"))
         classpath("com.diffplug.spotless:spotless-plugin-gradle:3.23.0")
         classpath("com.android.tools:r8:1.5.42")
