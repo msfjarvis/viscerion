63a85a666027909cb14340f70fec776538b34744
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-19 23:36:12 +0530

    Dependency updates
    Kotlin 1.3.40
    R8 1.5.51
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0-beta04")
-        classpath(kotlin("gradle-plugin", "1.3.31"))
+        classpath(kotlin("gradle-plugin", "1.3.40"))
         classpath("com.diffplug.spotless:spotless-plugin-gradle:3.23.0")
-        classpath("com.android.tools:r8:1.5.42")
+        classpath("com.android.tools:r8:1.5.51")
     }
 }
 
