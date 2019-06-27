c6bcba9562942dd2b187ad5aabc32a43797c5544
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-27 15:15:26 +0530

    Dependency updates
    AGP 3.5.0-beta05
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         maven(url = "https://storage.googleapis.com/r8-releases/raw")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0-beta04")
+        classpath("com.android.tools.build:gradle:3.5.0-beta05")
         classpath(kotlin("gradle-plugin", "1.3.40"))
         classpath("com.android.tools:r8:1.5.52")
     }
