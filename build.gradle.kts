a6a6b6ac37357f4baec7b7d3547c2ee82bb77a99
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-28 23:30:37 +0530

    Dependency updates
    Gradle 5.5
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks {
         reportfileName = "report"
     }
     named<Wrapper>("wrapper") {
-        gradleVersion = "5.5-rc-4"
+        gradleVersion = "5.5"
         distributionType = Wrapper.DistributionType.ALL
     }
 }
