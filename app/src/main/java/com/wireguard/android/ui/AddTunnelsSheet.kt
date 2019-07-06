msfjarvis/viscerion

commit 8bfb5db935d0613cfd9b99c088c9e259883be291

global: Remove inline-activity-result
Has been causing memory leaks and sporadic crashes for me.

Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/ui/AddTunnelsSheet.kt
@@ -13,8 +13,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.FrameLayout
-import androidx.appcompat.app.AppCompatActivity
-import com.afollestad.inlineactivityresult.startActivityForResult
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -24,7 +22,6 @@ import com.wireguard.android.R
 import com.wireguard.android.activity.TunnelCreatorActivity
 import com.wireguard.android.fragment.TunnelListFragment
 import com.wireguard.android.util.resolveAttribute
-import timber.log.Timber
 import com.google.android.material.R as materialR
 
 class AddTunnelsSheet(val fragment: TunnelListFragment) : BottomSheetDialogFragment() {
@@ -89,12 +86,7 @@ class AddTunnelsSheet(val fragment: TunnelListFragment) : BottomSheetDialogFragm
             addCategory(Intent.CATEGORY_OPENABLE)
             type = "*/*"
         }
-        startActivityForResult(Intent.createChooser(intent, "Choose ZIP or conf")) { _, data ->
-            data.data?.also { uri ->
-                Timber.tag("TunnelImport").i("Import uri: $uri")
-                fragment.importTunnel(uri)
-            }
-        }
+        fragment.startActivityForResult(Intent.createChooser(intent, "Choose ZIP or conf"), TunnelListFragment.REQUEST_IMPORT)
     }
 
     private fun onRequestScanQRCode() {
@@ -103,16 +95,6 @@ class AddTunnelsSheet(val fragment: TunnelListFragment) : BottomSheetDialogFragm
             setBeepEnabled(false)
             setPrompt(getString(R.string.qr_code_hint))
         }
-        startActivityForResult(intentIntegrator.createScanIntent()) { success, data ->
-            IntentIntegrator.parseActivityResult(
-                    when (success) {
-                        true -> AppCompatActivity.RESULT_OK
-                        else -> AppCompatActivity.RESULT_CANCELED
-                    },
-                    data
-            )?.contents?.let {
-                fragment.importTunnel(it)
-            }
-        }
+        startActivityForResult(intentIntegrator.createScanIntent(), IntentIntegrator.REQUEST_CODE)
     }
 }

app/build.gradle.kts
@@ -114,7 +114,6 @@ dependencies {
     implementation(deps.AndroidX.sliceCore)
     implementation(deps.AndroidX.sliceKtx)
     implementation(deps.Material.material)
-    implementation(deps.ThirdParty.inlineActivityResult)
     implementation(deps.ThirdParty.koinAndroid)
     implementation(deps.ThirdParty.koinCore)
     implementation(deps.ThirdParty.recyclical)

app/src/main/java/com/wireguard/android/activity/LogViewerActivity.kt
@@ -15,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.afollestad.inlineactivityresult.startActivityForResult
 import com.afollestad.recyclical.datasource.dataSourceTypedOf
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
@@ -67,21 +66,24 @@ class LogViewerActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RESULT_OK && requestCode == REQUEST_LOCATION) {
+            data?.data?.also { uri ->
+                Timber.d("Exporting logcat stream to ${uri.path}")
+                exportLog(uri)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     private fun createLogFile() {
         val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
             type = "text/plain"
             putExtra(Intent.EXTRA_TITLE, "viscerion-log.txt")
         }
-
-        startActivityForResult(intent) { result, data ->
-            if (result) {
-                data.data?.also { uri ->
-                    Timber.d("Exporting logcat stream to ${uri.path}")
-                    exportLog(uri)
-                }
-            }
-        }
+        startActivityForResult(intent, REQUEST_LOCATION)
     }
 
     private fun exportLog(fileUri: Uri) {
@@ -111,4 +113,8 @@ class LogViewerActivity : AppCompatActivity() {
     }
 
     data class LogEntry(val line: String)
+
+    companion object {
+        private const val REQUEST_LOCATION = 1000
+    }
 }

app/src/main/java/com/wireguard/android/fragment/BaseFragment.kt
@@ -6,12 +6,13 @@
 package com.wireguard.android.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import com.afollestad.inlineactivityresult.startActivityForResult
 import com.google.android.material.snackbar.Snackbar
 import com.wireguard.android.R
 import com.wireguard.android.activity.BaseActivity
@@ -58,6 +59,20 @@ abstract class BaseFragment : Fragment(), OnSelectedTunnelChangedListener {
         super.onDetach()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_VPN_PERMISSION && resultCode == AppCompatActivity.RESULT_OK) {
+            pendingTunnel?.let { tunnel ->
+                pendingTunnelUp?.let { tunnelUp ->
+                    setTunnelStateWithPermissionsResult(tunnel, tunnelUp)
+                }
+            }
+            pendingTunnel = null
+            pendingTunnelUp = null
+        }
+    }
+
     fun setTunnelState(view: View, checked: Boolean) {
         val binding = DataBindingUtil.findBinding<ViewDataBinding>(view)
         val tunnel: Tunnel?
@@ -75,17 +90,7 @@ abstract class BaseFragment : Fragment(), OnSelectedTunnelChangedListener {
                 intent?.let {
                     pendingTunnel = tunnel
                     pendingTunnelUp = checked
-                    startActivityForResult(it) { result, _ ->
-                        if (result) {
-                            pendingTunnel?.let { tunnel ->
-                                pendingTunnelUp?.let { tunnelUp ->
-                                    setTunnelStateWithPermissionsResult(tunnel, tunnelUp)
-                                }
-                            }
-                            pendingTunnel = null
-                            pendingTunnelUp = null
-                        }
-                    }
+                    startActivityForResult(it, REQUEST_CODE_VPN_PERMISSION)
                     return@thenAccept
                 }
             }
@@ -107,5 +112,9 @@ abstract class BaseFragment : Fragment(), OnSelectedTunnelChangedListener {
                 Toast.makeText(context, message, Toast.LENGTH_LONG).show()
             Timber.e(throwable)
         }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_VPN_PERMISSION = 23491
     }
 }

app/src/main/java/com/wireguard/android/fragment/TunnelListFragment.kt
@@ -5,6 +5,7 @@
  */
 package com.wireguard.android.fragment
 
+import android.content.Intent
 import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
@@ -18,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ActionMode
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
+import com.google.zxing.integration.android.IntentIntegrator
 import com.wireguard.android.R
 import com.wireguard.android.configStore.FileConfigStore.Companion.CONFIGURATION_FILE_SUFFIX
 import com.wireguard.android.databinding.ObservableKeyedRecyclerViewAdapter
@@ -160,6 +162,26 @@ class TunnelListFragment : BaseFragment() {
                     onTunnelImportFinished(tunnels, throwables)
                 }
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_IMPORT -> {
+                if (resultCode == AppCompatActivity.RESULT_OK)
+                    data?.data?.also { uri ->
+                        Timber.tag("TunnelImport").i("Import uri: $uri")
+                        importTunnel(uri)
+                    }
+                return
+            }
+            IntentIntegrator.REQUEST_CODE -> {
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.contents?.let {
+                    importTunnel(it)
+                }
+                return
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
@@ -425,5 +447,9 @@ class TunnelListFragment : BaseFragment() {
             val count = checkedItems.size
             mode.title = if (count == 0) "" else resources?.getQuantityString(R.plurals.delete_title, count, count)
         }
+    }
+
+    companion object {
+        const val REQUEST_IMPORT = 1
     }
 }

buildSrc/src/main/kotlin/DependencyStore.kt
@@ -42,7 +42,6 @@ class DependencyStore {
     }
 
     object ThirdParty {
-        private const val inlineActivityResultVersion = "0.1.0"
         private const val koinVersion = "2.0.1"
         private const val recyclicalVersion = "1.0.0"
         private const val retroFutureVersion = "1.7.1"
@@ -50,7 +49,6 @@ class DependencyStore {
         private const val timberVersion = "4.7.1"
         private const val zxingVersion = "3.4.0"
 
-        const val inlineActivityResult = "com.afollestad:inline-activity-result:$inlineActivityResultVersion"
         const val koinCore = "org.koin:koin-core:$koinVersion"
         const val koinAndroid = "org.koin:koin-android:$koinVersion"
         const val recyclical = "com.afollestad:recyclical:$recyclicalVersion"

commit 68655228e44279ee55e8306d39aa0af8d6f6e895

AddTunnelsSheet: Ensure activity result is delivered to the right target
Fixes: 8bfb5db935d0 ("global: Remove inline-activity-result")

Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/ui/AddTunnelsSheet.kt
@@ -104,6 +104,9 @@ class AddTunnelsSheet() : BottomSheetDialogFragment() {
             setBeepEnabled(false)
             setPrompt(getString(R.string.qr_code_hint))
         }
-        startActivityForResult(intentIntegrator.createScanIntent(), IntentIntegrator.REQUEST_CODE)
+        tunnelListFragment?.startActivityForResult(
+                intentIntegrator.createScanIntent(),
+                IntentIntegrator.REQUEST_CODE
+        )
     }
 }