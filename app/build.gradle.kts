db68fb3c91bcf4f103bf590a5236adddeb50c2c2
Harsh Shandilya <msfjarvis@gmail.com>
2019-07-24 11:45:42 +0530

    Enable German resources
    Ralf's been making significant progress with de localisation on
    Crowdin so I am comfortable enabling this earlier than I would normally do.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         versionCode = VersionConfiguration.versionCode
         versionName = VersionConfiguration.versionName
         if (System.getenv("DRONE") != "true") setProperty("archivesBaseName", "viscerion_${gitHash()}")
-        resConfigs("en", "fr", "ko", "pt-rBR", "ru")
+        resConfigs("de", "en", "fr", "ko", "pt-rBR", "ru")
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
