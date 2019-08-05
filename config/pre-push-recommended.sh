a005700cf97ca5bc428545fedb2a7323335f3cfd
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-05 21:41:21 +0530

    config: Update git hook to not test when source is not changed
    [ci skip]
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- config/pre-push-recommended.sh
+++ config/pre-push-recommended.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-./gradlew spotlessCheck
-./gradlew :app:assembleDebug
+
+if [[ $(git diff --name-only HEAD origin/$(git rev-parse --abbrev-ref HEAD) | grep -e '\.kt[s]\?$' -e '\.xml\?$') != "" ]]; then
+    gradle spotlessApply
+    gradle assembleDebug
+fi
+if [ $? -ne 0 ]; then exit 1; fi
