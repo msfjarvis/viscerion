a1825b63a4f47f74db9def0472e4bc6be8cddb71
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-09 13:48:04 +0530

    Bump AGP from 3.5.0-rc02 to 3.5.0-rc03
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         maven(url = "https://storage.googleapis.com/r8-releases/raw")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0-rc02")
+        classpath("com.android.tools.build:gradle:3.5.0-rc03")
         classpath(kotlin("gradle-plugin", "1.3.41"))
         classpath("com.android.tools:r8:1.5.66")
     }
