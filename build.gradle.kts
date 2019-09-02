7ad73f7d3ec33140bfb69f021a848362d3026459
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-02 06:51:58 +0530

    Bump R8 from 1.5.67 to 1.5.68
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0")
         classpath(kotlin("gradle-plugin", "1.3.50"))
-        classpath("com.android.tools:r8:1.5.67")
+        classpath("com.android.tools:r8:1.5.68")
     }
 }
 
