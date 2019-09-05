msfjarvis/viscerion

commit af99c34817cfa4982661e44aaf3b82eebaf168af

MainActivity: Fix memory leak on Q
https://twitter.com/Piwai/status/1169274622614704129
https://issuetracker.google.com/issues/139738913

Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/activity/MainActivity.kt
@@ -52,7 +52,11 @@ class MainActivity : BaseActivity(), FragmentManager.OnBackStackChangedListener 
             selectedTunnel = null
             return
         }
-        super.onBackPressed()
+        if (isTaskRoot) {
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onBackStackChanged() {

commit cf7885d8f8dedc81f0ce35a57e434b745cd8f29d

MainActivity: Fix navigation issues
Fixes: af99c34817cf ("MainActivity: Fix memory leak on Q")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/activity/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : BaseActivity(), FragmentManager.OnBackStackChangedListener 
         val backStackEntries = supportFragmentManager.backStackEntryCount
         // If the two-pane layout does not have an editor open, going back should exit the app.
         if (isTwoPaneLayout && backStackEntries <= 1) {
-            finish()
+            finishAfterTransition()
             return
         }
         // Deselect the current tunnel on navigating back from the detail pane to the one-pane list.
@@ -52,8 +52,8 @@ class MainActivity : BaseActivity(), FragmentManager.OnBackStackChangedListener 
             selectedTunnel = null
             return
         }
-        if (isTaskRoot) {
-            finishAfterTransition()
+        if (isTaskRoot && backStackEntries == 2) {
+            supportFragmentManager.popBackStack()
         } else {
             super.onBackPressed()
         }