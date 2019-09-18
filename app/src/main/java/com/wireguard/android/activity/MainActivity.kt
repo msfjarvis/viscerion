bd90223c560f418236d905fd2099249c3461155c
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-19 02:38:45 +0530

    MainActivity: Use Class<? extends Fragment> method for transactions
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/com/wireguard/android/activity/MainActivity.kt
+++ app/src/main/java/com/wireguard/android/activity/MainActivity.kt
@@ -98,7 +98,7 @@ class MainActivity : BaseActivity(), FragmentManager.OnBackStackChangedListener 
             }
             R.id.menu_action_edit -> {
                 supportFragmentManager.commit {
-                    replace(R.id.detail_container, TunnelEditorFragment())
+                    replace(R.id.detail_container, TunnelEditorFragment::class.java, null)
                     setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                     addToBackStack(null)
                 }
@@ -133,7 +133,7 @@ class MainActivity : BaseActivity(), FragmentManager.OnBackStackChangedListener 
         } else if (backStackEntries == 0) {
             // Create and show a new detail fragment.
             fragmentManager.commit {
-                add(R.id.detail_container, TunnelDetailFragment())
+                add(R.id.detail_container, TunnelDetailFragment::class.java, null)
                 setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 addToBackStack(null)
             }
