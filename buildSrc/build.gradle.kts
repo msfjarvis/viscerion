7001e1d0e598ef6b4886134010b8e7828c9a3bf4
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-19 21:14:30 +0530

    Update spotless
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,5 +17,5 @@ kotlinDslPluginOptions {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:3.24.3")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:3.25.0")
 }
