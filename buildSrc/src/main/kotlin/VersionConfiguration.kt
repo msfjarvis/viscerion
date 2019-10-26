92130b75846054a088a1f05a40b1010682b3ccd0
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-27 01:47:20 +0530

    Completely remove buildSrc (#193)
    * Completely remove buildSrc
    
    * Update spotless config
    
    * native: Configure cmake args at root level
    
    * spotless: Break out groovy rules into separate config
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildSrc/src/main/kotlin/VersionConfiguration.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © 2017-2019 WireGuard LLC.
- * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-object VersionConfiguration {
-    const val versionCode = 5250
-    const val versionName = "5.2.5"
-}
