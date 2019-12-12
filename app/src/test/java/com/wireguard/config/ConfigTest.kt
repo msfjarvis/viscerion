2a8871be54936a7c0ad90b034e504e78b52a6b6d
Harsh Shandilya <msfjarvis@gmail.com>
2019-12-12 18:16:58 +0530

    Convert ConfigTest to a unit test
    [ci skip]
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/test/java/com/wireguard/config/ConfigTest.kt
+++ app/src/test/java/com/wireguard/config/ConfigTest.kt
@@ -6,10 +6,9 @@
 package com.wireguard.config
 
 import android.content.Context
-import androidx.preference.PreferenceManager
-import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.SharedPreferences
 import com.wireguard.android.util.ApplicationPreferences
+import org.junit.After
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -18,34 +17,37 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
-import org.koin.test.AutoCloseKoinTest
+import org.koin.test.KoinTest
+import org.mockito.Mockito
 
-@LargeTest
-class ConfigTest : AutoCloseKoinTest() {
-    private lateinit var targetContext: Context
-    private lateinit var testContext: Context
-    private val testModule = module {
-        single { PreferenceManager.getDefaultSharedPreferences(androidContext()) }
-        single { ApplicationPreferences(get()) }
-    }
+class ConfigTest : KoinTest {
 
     @Before
-    fun init() {
-        targetContext = InstrumentationRegistry.getInstrumentation().targetContext
-        testContext = InstrumentationRegistry.getInstrumentation().context
-        stopKoin()
+    fun setupKoin() {
+        val sharedPrefs = Mockito.mock(SharedPreferences::class.java)
+        val context: Context = Mockito.mock(Context::class.java)
         startKoin {
-            androidContext(targetContext)
-            modules(testModule)
+            androidContext(context)
+            modules(module {
+                single { ApplicationPreferences(sharedPrefs) }
+            })
         }
     }
 
+    @After
+    fun cleanup() {
+        stopKoin()
+    }
+
     @Test
-    fun validConfigReadSuccessfully() {
-        val testConfig = testContext.assets.open("working.conf")
+    fun `test config parses correctly`() {
+        val testConfig = javaClass.classLoader!!.getResourceAsStream("working.conf")
         val config = Config.parse(testConfig)
         assertNotNull("Valid configs cannot not be null after parsing", config)
-        assertTrue("No applications should be excluded by default", config.`interface`.excludedApplications.isEmpty())
+        assertTrue(
+            "No applications should be excluded by default",
+            config.`interface`.excludedApplications.isEmpty()
+        )
         assertTrue("Test config has exactly one peer", config.peers.size == 1)
         assertTrue(
             "Test config's allowed IPs are 0.0.0.0/0 and ::0/0",
@@ -53,11 +55,14 @@ class ConfigTest : AutoCloseKoinTest() {
                 setOf(InetNetwork.parse("0.0.0.0/0"), InetNetwork.parse("::0/0"))
         )
         assertTrue("Test config has one DNS server", config.`interface`.dnsServers.size == 1)
-        assertTrue("", config.`interface`.dnsServers == setOf(InetAddressUtils.parse("193.138.218.74")))
+        assertTrue(
+            "",
+            config.`interface`.dnsServers == setOf(InetAddressUtils.parse("193.138.218.74"))
+        )
     }
 
     @Test(expected = BadConfigException::class)
-    fun invalidConfigThrows() {
-        Config.parse(testContext.assets.open("broken.conf"))
+    fun `broken config throws BadConfigException`() {
+        Config.parse(javaClass.classLoader!!.getResourceAsStream("broken.conf"))
     }
 }
