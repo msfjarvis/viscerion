ef5a9ff301df4ea4a2b0261a4ab69b8877bfc8d3
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-06 15:20:46 +0530

    global: Update copyrights
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- util/src/main/java/com/wireguard/util/SortedKeyedList.kt
+++ util/src/main/java/com/wireguard/util/SortedKeyedList.kt
@@ -1,6 +1,6 @@
 /*
- * Copyright © 2017-2019 WireGuard LLC.
- * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
+ * Copyright © 2017-2020 WireGuard LLC.
+ * Copyright © 2018-2020 Harsh Shandilya <msfjarvis@gmail.com>. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.wireguard.util
