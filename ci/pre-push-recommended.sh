d65c78362d2e4ba64271dc677c8ca8a57d100dbe
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-11 19:59:44 +0530

    ci/pre-push-recommended: Also run tests
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- ci/pre-push-recommended.sh
+++ ci/pre-push-recommended.sh
@@ -3,4 +3,5 @@
 if [[ $(git diff --name-only HEAD origin/$(git rev-parse --abbrev-ref HEAD) | grep -E 'buildSrc|native|app|crypto|config|util|gradle') != "" ]]; then
     gradle spotlessCheck || exit 1
     gradle assembleDebug || exit 1
+    gradle testDebug || exit 1
 fi
